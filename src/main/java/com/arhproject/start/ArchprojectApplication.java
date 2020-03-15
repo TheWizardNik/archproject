029ca8f536db21161a51e00faf2acf21fb2ee6b6
NikitaSpiridonov <nik-08-99@yandex.ru>
2020-03-15 18:15:45 +0400

    Initial commit

---
+++ src/main/java/com/arhproject/start/ArchprojectApplication.java
@@ -0,0 +1,13 @@
+package com.arhproject.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arhproject/start/ArchprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arhproject.start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
